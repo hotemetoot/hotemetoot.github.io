a205b90237694c8e5e527abb39a97bb3c802f035
hotemetoot <mf.alexander@gmail.com>
2016-12-22 20:31:46 -0300

    added mathjaxhelper
    mathjaxhelper.js in docs folder + added in .ym

---
+++ script/mathjaxhelper.js
@@ -0,0 +1,7 @@
+<script type="text/x-mathjax-config">
+MathJax.Hub.Config({
+  config: ["MMLorHTML.js"],
+  jax: ["input/TeX", "output/HTML-CSS", "output/NativeMML"],
+  extensions: ["MathMenu.js", "MathZoom.js"]
+});
+</script>
