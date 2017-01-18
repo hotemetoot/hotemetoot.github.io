3f956a57656b24ba42d17675b3783d7f2f1c69ee
hotemetoot <Alexander.Fernandes@Fernandes.sr>
2017-01-18 12:19:18 -0300

    practice update
    practice vraag herschreven, css van input aangepast op basis van id, if
    else statement opnieuw

--- javascripts/extra.js
+++ javascripts/extra.js
@@ -1,25 +1,67 @@
-$(document).ready(function(){
-    var speed = Math.floor(Math.random()* 8 + 1) *10;
-    var multiplier = Number(Math.round(Math.random()+"e+1")+"e-1")+ Math.floor(Math.random()*3+1);
-    var distance = +(multiplier*speed).toFixed(2) ;
-    var time = +(distance/speed).toFixed(2);
-    $("#speed").text(speed);
-    $("#distance").text(distance);
-    //$("#time").text(time);
-    $("#answer").text(time*60+" minutes"); 
-    $("#answer").hide(); 
-  $("#new").click(function(){
-    var speed = Math.floor(Math.random()* 8 + 1) *10;
-    var multiplier = Number(Math.round(Math.random()+"e+1")+"e-1")+ Math.floor(Math.random()*3+1);
-    var distance = +(multiplier*speed).toFixed(2) ;
-    var time = +(distance/speed).toFixed(2);
-    $("#speed").text(speed);
-    $("#distance").text(distance);
-    //$("#time").text(time);
-    $("#answer").text(time*60+" minutes"); 
-    $("#answer").hide(); 
-  });
-  $("#show").click(function(){
-    $("#answer").show();        
-  });
+// upon loading
+$(document).ready(function() {
+  var speed = Math.floor(Math.random() * 8 + 1) * 10;
+  var travelMin = Math.floor(Math.random() * 3 + 1) * 60 + Math.floor(Math.random() * 10) * 6;
+  var distance = +(travelMin * speed / 60).toFixed(2);
+  var startMin = Math.floor(Math.random() * 120 + 420);
+  var h0 = Math.floor(startMin / 60);
+  var m0 = startMin % 60;
+  if (m0 < 10) {
+    m0 = "0" + m0;
+  } else {
+    m0 = m0;
+  }
+  pause = Math.floor(Math.random() * 50 + 10);
+  var endMin = travelMin + startMin + pause;
+  var h1 = Math.floor(endMin / 60);
+  var m1 = endMin % 60;
+  if (m1 < 10) {
+    m1 = "0" + m1;
+  } else {
+    m1 = m1;
+  }
+  $("#speed").text(speed);
+  $("#distance").text(distance);
+  $("#t0").text(h0 + "." + m0);
+  $("#t1").text(h1 + "." + m1);
+  $("#solution").text(pause + " minutes");
+  $("#solution").hide();
+});
+// the check answer button
+$("#check").click(function() {
+  var answer = $("#input").val();
+  if (answer == pause) {
+    alert("Your answer is correct");
+  } else {
+    alert("Oops, try again");
+  }
+});
+// the new question button
+$("#new").click(function() {
+  var speed = Math.floor(Math.random() * 8 + 1) * 10;
+  var travelMin = Math.floor(Math.random() * 3 + 1) * 60 + Math.floor(Math.random() * 10) * 6;
+  var distance = +(travelMin * speed / 60).toFixed(2);
+  var startMin = Math.floor(Math.random() * 120 + 420);
+  var h0 = Math.floor(startMin / 60);
+  var m0 = startMin % 60;
+  if (m0 < 10) {
+    m0 = "0" + m0
+  };
+  pause = Math.floor(Math.random() * 50 + 10);
+  var endMin = travelMin + startMin + pause;
+  var h1 = Math.floor(endMin / 60);
+  var m1 = endMin % 60;
+  if (m1 < 10) {
+    m1 = "0" + m1
+  };
+  $("#speed").text(speed);
+  $("#distance").text(distance);
+  $("#t0").text(h0 + "." + m0);
+  $("#t1").text(h1 + "." + m1);
+  $("#solution").text(pause + " minutes");
+  $("#solution").hide();
+});
+// the show answer button
+$("#show").click(function() {
+  $("#solution").show();
 });
